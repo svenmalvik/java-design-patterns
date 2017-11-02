96e2075c93a2cddd224426b82928fa17a962890b
MALVIK Sven <sven@malvik.de>
2017-11-02 09:57:38 +0100

    Output to adoc

--- _docs/src/main/java/de/malvik/doc/Sysdoclet.java
+++ _docs/src/main/java/de/malvik/doc/Sysdoclet.java
@@ -2,15 +2,22 @@ package de.malvik.doc;
 
 import com.sun.javadoc.ClassDoc;
 import com.sun.javadoc.RootDoc;
+import io.github.swagger2markup.markup.builder.MarkupDocBuilder;
+import io.github.swagger2markup.markup.builder.MarkupDocBuilders;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+
+import static io.github.swagger2markup.markup.builder.MarkupLanguage.ASCIIDOC;
 
 /**
  * Created by smalvik on 30.10.2017.
  */
 public class Sysdoclet {
 
-    public static boolean start(RootDoc root){
+    public static boolean start(RootDoc root) {
         ClassDoc[] classDocs = root.classes();
         Arrays.stream(classDocs).forEach(classDoc -> process(classDoc));
         return true;
@@ -18,6 +25,9 @@ public class Sysdoclet {
 
     private static void process(ClassDoc classDoc) {
         String s = classDoc.commentText();
-        System.out.println(s);
+        MarkupDocBuilder docBuilders = MarkupDocBuilders.documentBuilder(ASCIIDOC);
+        docBuilders.sectionTitleLevel2(classDoc.qualifiedTypeName());
+        docBuilders.paragraph(s);
+        docBuilders.writeToFile(new File("c:\\tml\\index").toPath(), Charset.forName("UTF-8"), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
